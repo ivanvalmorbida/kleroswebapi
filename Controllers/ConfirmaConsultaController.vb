8805151f1ca9efab4ee35a762f0eb321eed2dbb1
celso trentini <trentini@kleros.com.br>
2025-09-26 18:53:33 -0300

    alterou sql

--- Controllers/ConfirmaConsultaController.vb
+++ Controllers/ConfirmaConsultaController.vb
@@ -32,7 +32,7 @@ Namespace Controllers
                 cn.execute("insert into TRILHA_AGENDA (MEDICO, Data, PERIODO, HORA, 
                 EVENTO, DATA_ALTERACAO, FUNCIONARIO, HISTORICO, TipoAgenda)
                 select MEDICO, DATA_CONSULTA, PERIODO, HORA, 4 evento, getdate() alterado, 
-                0 funcionario, ' Motivo: WhatsAPP Confirma' historico, 1 tipo from AGENDA_CLINICA
+                0 funcionario, 'Paciente: '+NOMEPACI+' Motivo: WhatsAPP Confirma' historico, 1 tipo from AGENDA_CLINICA
                 where id=" & obj.id)
 
             Else
