23486c29c46f499fa4c858912cce1542ba5ecbba
ivanvalmorbida <ivanvalmorbida@gmail.com>
2026-01-20 18:40:24 -0300

    ivan

--- Controllers/ConfirmaConsultaController.vb
+++ Controllers/ConfirmaConsultaController.vb
@@ -37,19 +37,19 @@ Namespace Controllers
 
             Else
                 'No retorno Cancelando consulta
-                cn.execute("Update AGENDA_CLINICA set NOMEPACI='',PACIENTE=0,TIPO_ATENDIMENTO=0,FONEPACI='',
+                cn.Execute("Update AGENDA_CLINICA set NOMEPACI='',PACIENTE=0,TIPO_ATENDIMENTO=0,FONEPACI='',
                 TONOMETRIA=0,MAPRETINA=0, ACUIDADE = 0,CONVENIO=0, STATUS='', 
                 SECRETARIA=0,OBSERVACAO='',REQUISICAO=0,REQUISICAO_TONO=0,REQUISICAO_MAP=0, 
                 REQUISICAO_ACUIDADE = 0,UTILIZADO=0, TIPO_LENTE_USO=0, TIPO_ATENDIMENTO_ABRANGE = -1, 
                 AUTORIZACAO = '', RG = '',GUIA=0, NRCONVENIO='', GONIOSCOPIA=0, MOTILIDADE= 0, 
                 REQUISICAO_GONIOSCOPIA= 0, REQUISICAO_MOTILIDADE= 0, CodigoClube=0, EsteticistaAtividade=0, 
-                CONVENIO_PLANO='' Where ID =" & obj.id)
+                CONVENIO_PLANO='' Where STATUS not in('FCH','FAT','AGU','ESP','PRE') and ID =" & obj.id)
 
-                cn.execute("insert into TRILHA_AGENDA (MEDICO, Data, PERIODO, HORA, 
+                cn.Execute("insert into TRILHA_AGENDA (MEDICO, Data, PERIODO, HORA, 
                 EVENTO, DATA_ALTERACAO, FUNCIONARIO, HISTORICO, TipoAgenda)
                 select MEDICO, DATA_CONSULTA, PERIODO, HORA, 4 evento, getdate() alterado, 
                 0 funcionario, 'Paciente: '+NOMEPACI+' Motivo: WhatsAPP Cancelou ' historico, 1 tipo from AGENDA_CLINICA
-                where id=" & obj.id)
+                where STATUS not in('FCH','FAT','AGU','ESP','PRE') and id=" & obj.id)
             End If
         End Sub
 
