32b9493b925b4eaae80671611771b26fcb8f237e
serviceX <serviceX@SERVERX>
2026-03-31 16:39:09 -0300

    implementação dos controllers, pacientecpf, convenios para web e medicos para web

---
+++ Controllers/MedicoWebController.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Data.SqlClient
+Imports System.Web.Http
+Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class cMedicoWeb
+        Public Property MedicoCodigo As Integer
+        Public Property MedicoNome As String
+        Public Property MedicoEspecialidade As String
+    End Class
+
+    Public Class MedicoWebController
+        Inherits ApiController
+
+        ' GET: api/ConvenioWeb
+        Public Function GetValues() As List(Of cMedicoWeb)
+            Dim sqlReader As SqlDataReader, strSQL As String, cn As New Conexao
+            Dim r As New List(Of cMedicoWeb)
+
+            strSQL = "Select codigo as MedicoCodigo, nome as MedicoNome, isnull(EspecialidadeAgendaWeb, '') as MedicoEspecialidade
+                from medico m where Tipo = 'M' and Ativo=-1 and AgendaWeb = -1 order by nome"
+
+            sqlReader = cn.OpenReader(strSQL)
+            While sqlReader.Read
+                r.Add(New cMedicoWeb() With {
+                    .MedicoCodigo = sqlReader("MedicoCodigo").ToString(),
+                    .MedicoEspecialidade = sqlReader("MedicoEspecialidade").ToString(),
+                    .MedicoNome = sqlReader("MedicoNome").ToString()
+                })
+            End While
+            sqlReader.Close()
+            cn.CloseConection()
+            Return r
+        End Function
+    End Class
+End Namespace
---
+++ Controllers/PacienteCPFController.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Data.SqlClient
+Imports System.Web.Http
+Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class cPacienteCPF
+        Public Property ConvenioCodigo As Integer
+        Public Property ConvenioNome As String
+        Public Property PacienteCelular As String
+        Public Property PacienteCodigo As Integer
+        Public Property PacienteNome As String
+        Public Property PacienteDataNascim As String
+
+    End Class
+
+    Public Class PacienteCPFController
+        Inherits ApiController
+
+        '' GET: api/PacienteCPF
+        'Public Function GetValues() As IEnumerable(Of String)
+        '    Return New String() {"value1", "value2"}
+        'End Function
+
+        ' GET: api/PacienteCPF/123456789
+        Public Function GetValue(ByVal id As Integer) As cPacienteCPF
+            Dim sqlReader As SqlDataReader, strSQL As String, cn As New Conexao
+            Dim sqlPar As New SqlParameter, colPar As New Collection
+            Dim r As New cPacienteCPF
+
+            sqlPar.DbType = DbType.String
+            sqlPar.Value = id
+            sqlPar.ParameterName = "@cpf"
+            colPar.Add(sqlPar)
+
+            strSQL = "select p.codigo as PacienteCodigo, p.NOME as PacienteNome, 
+                DATA_NASCIM as PacienteDataNascim, celular as PacienteCelular, 
+                c.codigo as ConvenioCodigo, c.nome as ConvenioNome 
+                from paciente p
+                inner join convenio c on c.codigo = CONVENIO
+                where CNPJCPF = @cpf PacienteCNPJCPF
+                order by DataChamada desc"
+
+            sqlReader = cn.OpenReaderWithParam(strSQL, colPar)
+            If sqlReader.Read Then
+                r.ConvenioCodigo = sqlReader("ConvenioCodigo").ToString()
+                r.ConvenioNome = sqlReader("ConvenioNome").ToString()
+                r.PacienteCelular = sqlReader("PacienteCelular").ToString()
+                r.PacienteCodigo = sqlReader("PacienteCodigo").ToString()
+                r.PacienteNome = sqlReader("PacienteNome").ToString()
+                r.PacienteDataNascim = sqlReader("PacienteDataNascim").ToString()
+            End If
+            sqlReader.Close()
+            cn.CloseConection()
+            Return r
+        End Function
+    End Class
+End Namespace
