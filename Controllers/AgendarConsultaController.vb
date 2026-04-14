6ba36d065ced9b2c0a89aa1de3b216bc4c73b9ae
serviceX <serviceX@SERVERX>
2026-04-14 11:09:33 -0300

    fixs and feats

---
+++ Controllers/AgendarConsultaController.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class AgendarConsultaController
+        Inherits Controller
+
+        ' GET: AgendarConsulta
+        Function Index() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
+
+
+'FALTA FAZER
+'* Se após a seleção do horário, este já tiver sido utilizado, informar o Paciente para selecionar novo horário.
+'Select Case AgendaNomePaci from Agenda_Clinica
+'where Medico = MedicoCodigoSelecionado And DataConsulta = DataSelecionada And Hora = HoraSelecionada And isnull(NomePaci, '') = ''
+'\* Se já tiver registro, informar o Paciente que o horário já está utilizado. Direcionar para escolha de novo horário.
+
+'* Update Agenda Clinica
+'update AGENDA_CLINICA
+'Set NOMEPACI = PacienteNome, CONVENIO = ConvenioCodigoSelecionado, Celular = PacienteCelular, OBSERVACAO = PacienteDataNascim, STATUS = 'WA1'
+', TIPO_ATENDIMENTO = 2, SECRETARIA = 0, TIPO_ATENDIMENTO_ABRANGE = 4, CNPJCPF = PacienteCNPJCPF, Nacionalidade = 0, CNPJCPF = PacienteCPF
+'where MEDICO = MedicoCodigoSelecionado And DATA_CONSULTA = DataSelecionada And HORA = HoraSelecionada
+
+'* Insert Trilha
+'insert into TRILHA_AGENDA (MEDICO, DATA, PERIODO, HORA, EVENTO, DATA_ALTERACAO, FUNCIONARIO, HISTORICO, TipoAgenda)
+'values(MedicoCodigoSelecionado, DataSelecionada, Case when HoraSelecionada < 1200 then 1 else 2 end, HoraSelecionada, 1, getdate(), 0, 
+''Anterior -> ' & StatusAnterior & 'Novo -> WhatsAPP Agenda Consulta, 1)
+
+'* Identifica os horários livres do Médico para agendamento (15 dias)
+' Select Case convert(Of Char, DATA_CONSULTA, 103) As AgendaDataConsulta
+' , case when DATEpart(WEEKDAY, DATA_CONSULTA) = 2 then 'Segunda' else case when DATEpart(WEEKDAY, DATA_CONSULTA) = 3 then 'Terça'  else case when DATEpart(WEEKDAY, DATA_CONSULTA) = 4 then 'Quarta' 
+'  Else Case When DATEpart(WEEKDAY, DATA_CONSULTA) = 5 Then 'Quinta' else case when DATEpart(WEEKDAY, DATA_CONSULTA) = 6 then 'Sexta' end end end end end as AgendaDiaSemana
+' , Período as AgendaPeriodo, Hora as AgendaHora, case when STATUS = '$$$' then 'Horário para Convênio Particular' else '' end as AgendaObservacao
+' --, STATUS, m.NOME as AgendaMedicoNome
+' from AGENDA_CLINICA
+' inner join medico m On m.CODIGO=MEDICO
+' left outer join PARAMETROS_GERAIS pg On pg.COD_PARTICULAR > 0
+' where medico = 17 And DATA_CONSULTA >= getdate()
+'  And DATA_CONSULTA <= getdate() + 15
+' And isnull(NOMEPACI, '') = ''
+' order by DATA_CONSULTA, HORA
--- Controllers/ConfirmaConsultaController.vb
+++ Controllers/ConfirmaConsultaController.vb
@@ -43,13 +43,13 @@ Namespace Controllers
                 REQUISICAO_ACUIDADE = 0,UTILIZADO=0, TIPO_LENTE_USO=0, TIPO_ATENDIMENTO_ABRANGE = -1, 
                 AUTORIZACAO = '', RG = '',GUIA=0, NRCONVENIO='', GONIOSCOPIA=0, MOTILIDADE= 0, 
                 REQUISICAO_GONIOSCOPIA= 0, REQUISICAO_MOTILIDADE= 0, CodigoClube=0, EsteticistaAtividade=0, 
-                CONVENIO_PLANO='' Where STATUS not in('FCH','FAT','AGU','ESP','PRE') and ID =" & obj.id)
+                CONVENIO_PLANO='' Where STATUS not in('FCH','FAT','AGU','ESP','PRE','CON') and ID =" & obj.id)
 
                 cn.Execute("insert into TRILHA_AGENDA (MEDICO, Data, PERIODO, HORA, 
                 EVENTO, DATA_ALTERACAO, FUNCIONARIO, HISTORICO, TipoAgenda)
                 select MEDICO, DATA_CONSULTA, PERIODO, HORA, 4 evento, getdate() alterado, 
                 0 funcionario, 'Paciente: '+NOMEPACI+' Motivo: WhatsAPP Cancelou ' historico, 1 tipo from AGENDA_CLINICA
-                where STATUS not in('FCH','FAT','AGU','ESP','PRE') and id=" & obj.id)
+                where STATUS not in('FCH','FAT','AGU','ESP','PRE','CON') and id=" & obj.id)
             End If
         End Sub
 
